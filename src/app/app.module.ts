095f9b2a5a1c261e4b6e9c733ad0070ae15a6d97
FedorKandiranda <YtCvj;tntEuflfnm!>
2022-05-17 16:48:13 +0300

    task1

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatListModule } from '@angular/material/list';
+import { MatBadgeModule } from '@angular/material/badge';
+import { InfoCompanyComponent } from './info-company/info-company.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSelectModule} from '@angular/material/select';
+import {MatRadioModule} from '@angular/material/radio';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InfoCompanyComponent,
+    ContactsComponent,
+    HeaderComponent,
+    MainComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatListModule,
+    MatBadgeModule,
+    MatTooltipModule,
+    MatTabsModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    MatRadioModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
