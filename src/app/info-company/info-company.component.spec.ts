016815b2d32f19588ec275da7d65544d347ebbef
FedorKandiranda <YtCvj;tntEuflfnm!>
2022-05-17 15:51:33 +0300

    task1

---
+++ src/app/info-company/info-company.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InfoCompanyComponent } from './info-company.component';
+
+describe('InfoCompanyComponent', () => {
+  let component: InfoCompanyComponent;
+  let fixture: ComponentFixture<InfoCompanyComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ InfoCompanyComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InfoCompanyComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
