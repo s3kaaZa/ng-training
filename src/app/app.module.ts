10e12f158ce7e7dc3825add1efd1e9de0b7a8629
FedorKandiranda <YtCvj;tntEuflfnm!>
2022-05-25 22:15:10 +0300

    work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './modules/users/components/header/header.component'; 
 import { UsersModule } from './modules/users/users.module';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HeaderComponent,
     UsersModule,
+    MatTabsModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
